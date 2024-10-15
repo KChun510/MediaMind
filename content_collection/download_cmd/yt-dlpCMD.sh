a34ae2b942f4117fa021266e128440ab6e4ffb2a
kyle chun <kyle_main@dhcp-172-27-088-015.mobile.uci.edu>
2024-10-15 13:24:03 -0700

    Working editing script, polishing is next

--- content_collection/download_cmd/yt-dlpCMD.sh
+++ content_collection/download_cmd/yt-dlpCMD.sh
@@ -1,3 +1,7 @@
 : 'yt-dlp --write-sub --write-auto-sub --sub-lang "en.*" <link>'
 
-yt-dlp --write-sub --write-auto-sub --sub-lang "en.*" https://www.youtube.com/watch?v=Q-TQQE1y68c
+yt-dlp --write-sub --write-auto-sub --sub-lang "en.*" https://www.youtube.com/watch?v=Q-TQQE1y68c -o "Q-TQQE1y68c"
+
+
+yt-dlp --write-sub --write-auto-sub --sub-lang "en.*" https://www.youtube.com/watch?v=si0Lp1SLHXg -o "si0Lp1SLHXg"
+
