abd42f470bb5ccdda4671d54190ee54233f427a8
kchun510 <westcoasttoast@WestCoaoastsMBP.lan>
2024-10-19 10:42:53 -0700

    Cleanup

--- editing/video_editing_scripts/node_examples.ts
+++
@@ -1,5 +0,0 @@
-// Bellow import is for sync calls. (We will need)
-const { execSync } = require('child_process');
-const ffmpeg = require('child_process').exec
-
-ffmpeg("ffmpeg -i input.mp4 -i input.mp3 -c copy -map 0:v:0 -map 1:a:0 output.mp4")
