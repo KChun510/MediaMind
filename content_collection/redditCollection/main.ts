0b8e7c9c8d68263ffa6679e37af65ea4d6253429
kchun510 <westcoasttoast@WestCoastToasts-MacBook-Pro.local>
2024-11-09 12:39:59 -0800

    Quick update

--- content_collection/redditCollection/main.ts
+++ content_collection/redditCollection/main.ts
@@ -55,7 +55,7 @@ function chunkFile(postID: string, postTitle: string, data: string) {
 }
 
 (async function() {
-	const reddit_post = await POST_Get_Reddit_Post(subReddits.DRAMA, 10)
+	const reddit_post = await POST_Get_Reddit_Post(subReddits.DRAMA, 1)
 	const invalid_post = await pullAllInvRedditIDs()
 	//	const reddit_post = (await POST_Get_Reddit_Post('nosleep', 4))
 	for (const post of reddit_post) {
