90d15fb27fb2852e63b9e45d67d5a37a625eb1bc
Kchun510 <kylechun1@gmail.com>
2024-11-16 18:19:04 -0800

    Removing an option from XTTS

--- tellTale/content_collection/redditCollection/main.ts
+++ tellTale/content_collection/redditCollection/main.ts
@@ -18,6 +18,7 @@ enum subReddits {
 	CRAZY_STORIES = 'crazystories',
 	DRAMA = 'SubredditDrama',
 	TECH_SUPPORT = 'talesfromtechsupport',
+	STORIES = 'stories',
 }
 
 const byteSize = (outStr: string) => new Blob([outStr]).size
@@ -74,7 +75,7 @@ async function create_metaData(input: { valid_file: string, text_cont: string })
 
 
 (async function() {
-	const reddit_post = await POST_Get_Reddit_Post(subReddits.DRAMA, 1)
+	const reddit_post = await POST_Get_Reddit_Post(subReddits.STORIES, 1)
 	const invalid_post = await pullAllInvRedditIDs()
 	//	const reddit_post = (await POST_Get_Reddit_Post('nosleep', 4))
 	for (const post of reddit_post) {
