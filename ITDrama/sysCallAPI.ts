2641c37ff2c228c2ab8bd79798c9c3ffb6787018
Kchun510 <kylechun1@gmail.com>
2024-11-16 17:21:05 -0800

    Localized error checking for XTTS

--- ITDrama/sysCallAPI.ts
+++ ITDrama/sysCallAPI.ts
@@ -3,6 +3,7 @@ const util = require('util')
 const readFile = util.promisify(fs.readFile)
 require('dotenv').config({ path: require('find-config')('.env') })
 import fs from 'fs';
+import { delRedditData } from './db_dir/db_actions'
 
 const CONT_DIR = process.env.CONT_DIR
 
@@ -102,12 +103,11 @@ export async function tts_coqui(valid_file: string, randomized: boolean, inputSp
                 const result = execSync(tts_string, { encoding: 'utf8' })
                 console.log("XTTS worked: ", result)
         } catch (error) {
-                const cmd_del_txt = `rm ${CONT_DIRS.Reddit_text}/${valid_file}`
-                console.error('Command failed!')
+                console.error('XTTS failed!')
                 console.error('Exit code:', error.status)
                 console.error('Stdout:', error.stdout?.toString())
                 console.error('Stderr:', error.stderr?.toString())
-                execSync(cmd_del_txt)
+                delRedditData((valid_file.split("."))[0])
                 process.exit(1)
         }
         console.log(`COQUI_AI: Audio content written to file: ${valid_file}.mp3`);
