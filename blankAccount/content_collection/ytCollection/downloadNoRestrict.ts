501637dcf7ef63197b9cb10cb2b032579fb4a2ef
Kchun510 <kylechun1@gmail.com>
2024-11-25 09:07:10 -0800

    Base setting for blank account, videoDuration for unristriced download equals long

--- blankAccount/content_collection/ytCollection/downloadNoRestrict.ts
+++ blankAccount/content_collection/ytCollection/downloadNoRestrict.ts
@@ -28,8 +28,7 @@ function videoTime(videoData: VIDEO_SQL_SCHEMA[]): number {
         const oAuthToken = await authorize(JSON.parse(content))
         while (totalVideoTime <= maxVideoTime) {
             try {
-                //const vidIdRes = await getVideosByKeyWords(oAuthToken, { valid_vids: 10, keywords: "First Person Shooter or forza gameplay HD", videoLicense: "any", results: 10 })
-                const vidIdRes = await getVideosByKeyWords(oAuthToken, { valid_vids: 10, keywords: "gameplay hd no commentary", videoLicense: "any", results: 10 })
+                const vidIdRes = await getVideosByKeyWords(oAuthToken, { valid_vids: 10, keywords: "gameplay hd no commentary", videoLicense: "any", results: 10, videoDuration: "long" })
                 const videoDetails = await getVideoDetails(oAuthToken, vidIdRes)
                 for (const video of videoDetails ?? []) {
                     const videoCommand = `yt-dlp --sub-lang "en.*" --embed-subs --no-overwrites https://www.youtube.com/watch?v=${video.videoID} -o "${outPutPath}/videos/${video.videoID}"`
