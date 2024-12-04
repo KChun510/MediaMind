a2b5ce2fef63e4d87babfd10faef3f87d617df73
kchun510 <kylechun1@gmail.com>
2024-12-03 20:32:54 -0800

    Updating

--- blankAccount/editing/main.ts
+++ blankAccount/editing/main.ts
@@ -157,7 +157,7 @@ async function twoVidsPlusStory() {
 
         while (video1Time < storyQTime) {
 
-                while (videoTime([globalVideoQ[0]]) < storyQTime || globalVideoQ.length < 1) {
+                while (globalVideoQ.length < 1 || videoTime([globalVideoQ[0]]) < storyQTime) {
                         if (globalVideoQ.length < 1) {
                                 await downloadYTVideo()
                                 globalVideoQ = await selectAllFromVideo(10)
@@ -179,12 +179,11 @@ async function twoVidsPlusStory() {
         }
 
         while (video2Time < storyQTime) {
-                while (videoTime([globalVideoQ[0]]) < storyQTime || globalVideoQ.length < 1) {
-
+                while (globalVideoQ.length < 1 || (videoTime([globalVideoQ[0]]) < storyQTime || globalVideoQ[0].videoID in globalVideoQLog)) {
                         if (globalVideoQ.length < 1) {
                                 await downloadYTVideo()
                                 globalVideoQ = (await selectAllFromVideo(10)).filter(obj => !(obj.videoID in globalVideoQ));
-                        } else if (videoTime([globalVideoQ[0]]) < storyQTime) {
+                        } else if (videoTime([globalVideoQ[0]]) < storyQTime || globalVideoQ[0].videoID in globalVideoQLog) {
                                 videoCleanUp(globalVideoQ[0].videoID)
                                 await downloadYTVideo()
                                 globalVideoQ = (await selectAllFromVideo(10)).filter(obj => !(obj.videoID in globalVideoQ));
