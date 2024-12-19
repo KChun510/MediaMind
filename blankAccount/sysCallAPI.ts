1e71726745d9bc284dc08ccdbc42af3ab800176e
Kchun510 <kylechun1@gmail.com>
2024-12-19 10:47:53 -0800

    Re-add the syscall

--- blankAccount/sysCallAPI.ts
+++ blankAccount/sysCallAPI.ts
@@ -10,6 +10,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 const CONT_DIR = process.env.CONT_DIR
 
 const CONT_DIRS = {
+        overlay_png: `${CONT_DIR}/overlay_png`,
         Reddit_audio: `${CONT_DIR}/reddit_cont/audio_dir`,
         Reddit_sub: `${CONT_DIR}/reddit_cont/sub_dir`,
         Reddit_text: `../content_collection/redditCollection/reddit_content_format/input_content/text_storys`,
