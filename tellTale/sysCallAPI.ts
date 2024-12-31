900f72d797f6e74d2e90268fd0fb5d7ea9339a05
Kchun510 <kylechun1@gmail.com>
2024-12-30 20:48:48 -0800

    fixing typo one space

--- tellTale/sysCallAPI.ts
+++ tellTale/sysCallAPI.ts
@@ -39,7 +39,7 @@ export function create_story_over_single_video(rPostId: string, ytVideoId: strin
 
 export function create_twoVids_OneStory(rPostId: string, ytVideoId1: string, ytVideoId2: string) {
 
-        const cmd_twoVids_OneStory = `ffmpeg -y -i "${CONT_DIRS.ytVideos}/${ytVideoId1}.mp4" -i "${CONT_DIRS.ytVideos}/${ytVideoId2}.mp4"-i "${CONT_DIRS.Reddit_audio}/${rPostId}.txt.mp3" -filter_complex "[0:v]scale=1080:960:force_original_aspect_ratio=increase,crop=1080:960[subtop];[1:v]scale=1080:960:force_original_aspect_ratio=increase,crop=1080:960[subbottom];[subtop][subbottom]vstack[stacked];[stacked]ass=${CONT_DIRS.Reddit_sub}/${rPostId}.ass[stacked_with_subs]" -map "[stacked_with_subs]" -map 2:a -c:v libx264 -c:a aac -b:a 192k -preset fast -crf 23 -shortest "${CONT_DIRS.prodVidAndStory}/${rPostId}.mp4"`
+        const cmd_twoVids_OneStory = `ffmpeg -y -i "${CONT_DIRS.ytVideos}/${ytVideoId1}.mp4" -i "${CONT_DIRS.ytVideos}/${ytVideoId2}.mp4" -i "${CONT_DIRS.Reddit_audio}/${rPostId}.txt.mp3" -filter_complex "[0:v]scale=1080:960:force_original_aspect_ratio=increase,crop=1080:960[subtop];[1:v]scale=1080:960:force_original_aspect_ratio=increase,crop=1080:960[subbottom];[subtop][subbottom]vstack[stacked];[stacked]ass=${CONT_DIRS.Reddit_sub}/${rPostId}.ass[stacked_with_subs]" -map "[stacked_with_subs]" -map 2:a -c:v libx264 -c:a aac -b:a 192k -preset fast -crf 23 -shortest "${CONT_DIRS.prodVidAndStory}/${rPostId}.mp4"`
 
         console.log(execSync(cmd_twoVids_OneStory, { encoding: 'utf-8' }).toString())
 }
