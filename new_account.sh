dd610aab03a33519fcb1387f0fc3d3e3726d69d9
Kchun510 <kylechun1@gmail.com>
2025-01-05 15:44:42 -0800

    Working state for prod

--- new_account.sh
+++ new_account.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONT_DIR="$HOME/Desktop/content_for_system/"
+CONT_DIR="/mnt/md0/work_station/content_for_system"
 read -p "Name of new Account: " name
 read -p $'What kind of account: \n1) Text_based \n2) Video_based\nEnter your option: ' accountType
 
