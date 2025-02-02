c4f9b733ba4c094da3cd2ff85326b8cc30206c29
Kchun510 <kylechun1@gmail.com>
2025-02-02 11:24:02 -0800

    Current state of prod

--- cop.action.news/content_collection/ytCollection/downloadNoRestrict.ts
+++ cop.action.news/content_collection/ytCollection/downloadNoRestrict.ts
@@ -83,8 +83,7 @@ async function create_metaData(input: { videoID: string, videoName: string }) {
                 const vidIdRes = await getVideosByKeyWords(oAuthToken, { valid_vids: 10, keywords: "police footage news", videoLicense: "any", results: 50, videoDuration: "long" })
                 const videoDetails = await getVideoDetails(oAuthToken, vidIdRes)
                 for (const video of videoDetails ?? []) {
-                    const videoCommand = `yt-dlp -f "bv*[height=1080][width=1920][ext=mp4]+ba[ext=m4a]/bv*[width=1280][ext=mp4]/bv*[width=854][ext=mp4]+ba[ext=m4a]/b[ext=mp4]" --write-sub --write-auto-sub --sub-lang "en.*" --embed-subs --force-overwrites -o "${outPutPath}/video_dir/${video.videoID}.%(ext)s" "https://www.youtube.com/watch?v=${video.videoID}"`;
-
+                    const videoCommand = `yt-dlp -f "bv*[height=1080][ext=mp4]+ba[ext=m4a]/bv*[height=720][ext=mp4]+ba[ext=m4a]/bv*[height=480][ext=mp4]+ba[ext=m4a]/b[ext=mp4]" --merge-output-format mp4 --write-sub --write-auto-sub --sub-lang "en.*" --embed-subs --force-overwrites -o "${outPutPath}/video_dir/${video.videoID}.%(ext)s" "https://www.youtube.com/watch?v=${video.videoID}"`;
 
                     const subtitleCommand = `ffmpeg -y -i "${outPutPath}/video_dir/${video.videoID}.mp4" -map 0:s:0? "${outPutPath}/srt/${video.videoID}.srt"`
 
--- cop.action.news/content_collection/ytCollection/main.ts
+++ cop.action.news/content_collection/ytCollection/main.ts
@@ -44,7 +44,8 @@ async function DownloadNotNeeded(): Promise<boolean> {
                 const vidIdRes = await getVideosByKeyWords(oAuthToken, { valid_vids: 10, keywords: "cop footage", videoLicense: "any", results: 50 })
                 const videoDetails = await getVideoDetails(oAuthToken, vidIdRes)
                 for (const video of videoDetails ?? []) {
-                    const videoCommand = `yt-dlp -f "bv*[height=1080][width=1920][ext=mp4]+ba[ext=m4a]/bv*[width=1280][ext=mp4]/bv*[width=854][ext=mp4]+ba[ext=m4a]/b[ext=mp4]" --write-sub --write-auto-sub --sub-lang "en.*" --embed-subs --force-overwrites -o "${outPutPath}/video_dir/${video.videoID}.%(ext)s" "https://www.youtube.com/watch?v=${video.videoID}"`;
+                    const videoCommand = `yt-dlp -f "bv*[height=1080][ext=mp4]+ba[ext=m4a]/bv*[height=720][ext=mp4]+ba[ext=m4a]/bv*[height=480][ext=mp4]+ba[ext=m4a]/b[ext=mp4]" --merge-output-format mp4 --write-sub --write-auto-sub --sub-lang "en.*" --embed-subs --force-overwrites -o "${outPutPath}/video_dir/${video.videoID}.%(ext)s" "https://www.youtube.com/watch?v=${video.videoID}"`;
+
 
                     const currVidTime = videoTime([video])
                     if (totalVideoTime >= maxVideoTime) {
